c1d4c3d3817fde478531ae125757b97a7d629558
kittikun <earthyo.wtf@gmail.com>
2019-10-10 14:35:53 +0700

    video 20 kotlintutorail @14:35 10/10/2562

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -1,17 +1,14 @@
 import com.example.kotlintutorial.*
 
 fun main(args: Array<String>){
-    val enemy = Enemy(name = "test enemy", hitPoints = 10, lives = 3)
-    println(enemy)
-
-    enemy.takeDamage(damage = 4)
-    println(enemy)
-
-    enemy.takeDamage(damage = 11)
-    print(enemy)
 
     val uglyTroll = Troll(name = "Ugly Troll")
     println(uglyTroll)
     uglyTroll.takeDamage(damage = 30)
     println(uglyTroll)
+
+    val vlad = Vampyre(name = "Vlad")
+    println(vlad)
+    vlad.takeDamage(8)
+    println(vlad)
 }
