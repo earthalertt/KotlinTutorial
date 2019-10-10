4cdd532e2243eab99a1dc50019b412c9948b9df6
kittikun <earthyo.wtf@gmail.com>
2019-10-10 14:28:18 +0700

    video 19 kotlintutorail @14:28 10/10/2562

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -1,9 +1,17 @@
-import com.example.kotlintutorial.Loot
-import com.example.kotlintutorial.LootType
-import com.example.kotlintutorial.Player
-import com.example.kotlintutorial.Weapon
+import com.example.kotlintutorial.*
 
 fun main(args: Array<String>){
+    val enemy = Enemy(name = "test enemy", hitPoints = 10, lives = 3)
+    println(enemy)
 
+    enemy.takeDamage(damage = 4)
+    println(enemy)
 
+    enemy.takeDamage(damage = 11)
+    print(enemy)
+
+    val uglyTroll = Troll(name = "Ugly Troll")
+    println(uglyTroll)
+    uglyTroll.takeDamage(damage = 30)
+    println(uglyTroll)
 }
