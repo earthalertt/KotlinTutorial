5a02f23a854f0ae417224eb12e3ef0fc1ee3ed52
kittikun <earthyo.wtf@gmail.com>
2019-10-10 10:21:47 +0700

    video 6 kotlintutorail @10:21 10/10/2562

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -8,21 +8,24 @@ fun main(args: Array<String>) {
 
     var timWeeklySalary: Int = 32
     var timMonthlySaraly: Int = timWeeklySalary * 4
-    println(timWeeklySalary)
-    println(timMonthlySaraly)
+    println("Tim;s $ weekly salary is $$timWeeklySalary")
+    println("Monthly, that comes to $timMonthlySaraly")
 
     println()
 
     val apples: Int = 6
     val orange: Int = 5
     val fruit: Int = apples - orange
-    println(fruit)
+    println("$apples apples - $orange orange leaves $fruit piece(s) of fruit")
 
-    println(apples / 4)
+    println("A quarter of the apples is ${apples / 4}")
 
     println()
-    val week: Int = 130
+    val week: Int = 234
     val years: Double = week / 52.0
-    println(years)
+    println("$week weeks is $years years")
+
+    println("My name is $tim")
+    println("I can print \$tim")
 
 }
