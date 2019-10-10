723a74be60b6aa0bb0e24e5efb4befca89de329b
kittikun <earthyo.wtf@gmail.com>
2019-10-10 10:13:00 +0700

    video 3 kotlintutorail @10:12 10/10/2562

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -1,12 +1,4 @@
-package com.example.kotlintutorial
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+fun main(args: Array<String>) {
+    println("Hello World")
+    println("My first Kotlin program")
 }
