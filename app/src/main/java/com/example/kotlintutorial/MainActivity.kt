b40a51ddb4295dbaf3ce3b3765b1ebdaeda77008
PC <earthyo.wtF@gmail.com>
2019-10-10 18:51:37 +0700

    video 23 @18:51 10/10/2019

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -2,7 +2,7 @@ import com.example.kotlintutorial.*
 
 fun main(args: Array<String>){
 
-    val uglyTroll = Troll(name = "Ugly Troll")
+    /*val uglyTroll = Troll(name = "Ugly Troll")
     println(uglyTroll)
     uglyTroll.takeDamage(damage = 30)
     println(uglyTroll)
@@ -11,18 +11,36 @@ fun main(args: Array<String>){
     println(vlad)
     vlad.takeDamage(8)
     println(vlad)
+    */
 
-    val dracula = VampyreKing(name = "Dracula")
-    println(dracula)
-    while (dracula.lives > 0) {
-        if (dracula.dodges()){
-            continue;
+    for (i in 1 .. 10){
+        val dracula = VampyreKing(name = "Dracula")
+        println(dracula)
+        while (dracula.lives > 0) {
+            if (dracula.dodges()){
+                continue;
+            }
+            if (dracula.runAway()){
+                println("Dracula ran awy")
+                break
+            }else {
+                dracula.takeDamage(80)
+            }
         }
-        if (dracula.runAway()){
-            println("Dracula ran awy")
-            break
-        }else {
-            dracula.takeDamage(12)
-        }
+        println("---------------------------")
     }
+
+
+    /*val conan = Player(name = "Conan")
+    conan.getLoot(Loot("Invisibility", LootType.POTION, 4.0))
+    conan.getLoot(Loot("Mithril", LootType.ARMOR, 183.0))
+    conan.getLoot(Loot("Ring of speed", LootType.RING, 25.0))
+    conan.getLoot(Loot("Red Potion", LootType.POTION, 2.0))
+    conan.getLoot(Loot("Cursed Shield", LootType.ARMOR, -8.0))
+    conan.getLoot(Loot("Brass Ring", LootType.RING, 1.0))
+    conan.getLoot(Loot("Chain Mail", LootType.ARMOR, 4.0))
+    conan.getLoot(Loot("Gold Ring", LootType.RING, 12.0))
+    conan.getLoot(Loot("Health Potion", LootType.POTION, 3.0))
+    conan.getLoot(Loot("Silver Ring", LootType.RING, 6.0))
+    conan.showInventory()*/
 }
