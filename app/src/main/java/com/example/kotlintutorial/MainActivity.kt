81b78167fda0268a7f83c96d006d90e6889a2d8a
kittikun <earthyo.wtf@gmail.com>
2019-10-10 10:18:10 +0700

    video 5 kotlintutorail @10:18 10/10/2562

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -6,8 +6,23 @@ fun main(args: Array<String>) {
     tim = "Tim Buchalka"
     println(tim)
 
-    var timSalary: Int = 32
-    var monthly: Int = timSalary * 4
-    println(timSalary)
-    println(monthly)
+    var timWeeklySalary: Int = 32
+    var timMonthlySaraly: Int = timWeeklySalary * 4
+    println(timWeeklySalary)
+    println(timMonthlySaraly)
+
+    println()
+
+    val apples: Int = 6
+    val orange: Int = 5
+    val fruit: Int = apples - orange
+    println(fruit)
+
+    println(apples / 4)
+
+    println()
+    val week: Int = 130
+    val years: Double = week / 52.0
+    println(years)
+
 }
