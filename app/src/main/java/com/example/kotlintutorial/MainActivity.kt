672a9ea13c9a43904648cedff2d8728c65e47eb8
kittikun <earthyo.wtf@gmail.com>
2019-10-10 10:53:15 +0700

    video 10 kotlintutorail @10:53 10/10/2562

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -1,21 +1,34 @@
 import com.example.kotlintutorial.Player
+import com.example.kotlintutorial.Weapon
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>){
+    val tim = Player(name = "Tim")
+    tim.show()
 
-    fun main(args: Array<String>){
-        val tim = Player(name = "Tim")
-        /*println(tim.name)
-    println(tim.lives)
-    println(tim.level)
-    println(tim.score)*/
-        tim.show()
+    val louise = Player(name = "Louise", level = 5)
+    louise.show()
 
-        val  louise = Player(name = "Louise", level =  5)
-        louise.show()
+    val gr8 = Player(name = "gr8", level = 4, lives = 8)
+    val one2watch = Player(name = "Ace", level = 2, lives = 5, score = 1000)
+    gr8.show()
+    one2watch.show()
+    println(one2watch.weapon.name.toUpperCase())
+    println(one2watch.weapon.damageInflicted)
 
-        val gr8 = Player(name = "gr8", level = 4, lives = 8)
-        val one2watch = Player(name = "Ace", level = 2, lives = 5, score = 1000)
-        gr8.show()
-        one2watch.show()
-    }
+    val axe = Weapon(name = "Axe", damageInflicted = 12)
+    gr8.weapon = axe
+    println(gr8.weapon.name)
+    println(axe.name)
+
+    axe.damageInflicted = 24
+    println(axe.damageInflicted)
+    println(gr8.weapon.damageInflicted)
+
+    tim.weapon = Weapon(name = "Sword", damageInflicted = 10)
+    println(tim.weapon.name)
+
+    tim.weapon = Weapon(name = "Spear", damageInflicted = 14)
+    println(tim.weapon.name)
+
+
 }
--- app/src/main/java/com/example/kotlintutorial/Player.kt
+++ app/src/main/java/com/example/kotlintutorial/Player.kt
@@ -1,6 +1,8 @@
 package com.example.kotlintutorial
 
 class Player (val name: String, val level: Int = 1, var  lives: Int = 3, var score: Int = 0){
+    var weapon: Weapon = Weapon(name = "Fist", damageInflicted = 1)
+
     fun  show(){
         println("""
             name: $name
